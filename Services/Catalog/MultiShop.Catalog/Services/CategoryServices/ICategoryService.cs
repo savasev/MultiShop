f8bb358e2f017ef4f558f0e04974dee4ea5e083a
savasevoutlook <savas.ev@outlook.com>
2025-05-21 23:06:13 +0300

    .

---
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Catalog.DTOs.CategoryDTOs;
+
+namespace MultiShop.Catalog.Services.CategoryServices;
+
+public interface ICategoryService
+{
+    Task<List<ResultCategoryDto>> GetAllCategoriesAsync();
+
+    Task<GetByIdCategoryDto> GetCategoryByIdAsync(string id);
+
+    Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+
+    Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+
+    Task DeleteCategoryAsync(string id);
+}
