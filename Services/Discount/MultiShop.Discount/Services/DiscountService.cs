971dd4960d6f6e37b389437667869477ba42fe89
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-24 21:43:28 +0300

    .

--- Services/Discount/MultiShop.Discount/Services/DiscountService.cs
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -1,14 +1,42 @@
-﻿using MultiShop.Discount.DTOs;
+﻿using Dapper;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.DTOs;
 
 namespace MultiShop.Discount.Services;
 
 public class DiscountService : IDiscountService
 {
+    #region Fields
+
+    private readonly DapperContext _dapperContext;
+
+    #endregion
+
+    #region Ctor
+
+    public DiscountService(DapperContext dapperContext)
+    {
+        _dapperContext = dapperContext;
+    }
+
+    #endregion
+
     #region Methods
 
-    public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+    public async Task CreateCouponAsync(CreateCouponDto createCouponDto)
     {
-        throw new NotImplementedException();
+        var query = "INSERT INTO Coupons(Code, Rate, IsActive, ValidDate) VALUES (@code, @rate, @isActive, @validDate)";
+
+        var parameters = new DynamicParameters();
+        parameters.Add("@code", createCouponDto.Code);
+        parameters.Add("@rate", createCouponDto.Rate);
+        parameters.Add("@isActive", createCouponDto.IsActive);
+        parameters.Add("@validDate", createCouponDto.ValidDate);
+
+        using (var connection = _dapperContext.CreateConnection())
+        {
+            await connection.ExecuteAsync(query, parameters);
+        }
     }
 
     public Task DeleteCouponAsync(int couponId)
