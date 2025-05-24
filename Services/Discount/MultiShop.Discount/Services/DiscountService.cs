1d7d390849af275d7db8fdea9cf054b3588e470d
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-24 21:27:29 +0300

    .

---
+++ Services/Discount/MultiShop.Discount/Services/DiscountService.cs
@@ -0,0 +1,35 @@
+﻿using MultiShop.Discount.DTOs;
+
+namespace MultiShop.Discount.Services;
+
+public class DiscountService : IDiscountService
+{
+    #region Methods
+
+    public Task CreateCouponAsync(CreateCouponDto createCouponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCouponAsync(int couponId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ResultCouponDto>> GetAllCouponsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetByIdCouponDto> GetCouponByIdAsync(int couponId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCouponAsync(UpdateCouponDto updateCouponDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
---
+++ Services/Discount/MultiShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using MultiShop.Discount.DTOs;
+
+namespace MultiShop.Discount.Services;
+
+public interface IDiscountService
+{
+    Task<List<ResultCouponDto>> GetAllCouponsAsync();
+
+    Task CreateCouponAsync(CreateCouponDto createCouponDto);
+
+    Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+
+    Task DeleteCouponAsync(int couponId);
+
+    Task<GetByIdCouponDto> GetCouponByIdAsync(int couponId);
+}
