672af74460c3bcfca10533eb1aef4f10f3ecb462
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-24 21:21:02 +0300

    .

--- Services/Discount/MultiShop.Discount/Context/DapperContext.cs
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -1,7 +1,27 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+using System.Data;
 
 namespace MultiShop.Discount.Context;
 
 public class DapperContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+    private readonly string _connectionString;
+
+    public DapperContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _connectionString = _configuration.GetConnectionString("DefaultConnection");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=ASUS-ROGS-Z590A;Initial Catalog=MultiShopDiscountDb;Integrated Security=true");
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+
+    public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
 }
---
+++ Services/Discount/MultiShop.Discount/DTOs/CreateCouponDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Discount.DTOs;
+
+public class CreateCouponDto
+{
+    public string Code { get; set; }
+
+    public int Rate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime ValidDate { get; set; }
+}
