7a210aede9aa4544130b5a4da0bb905fbc92f0fb
savasevoutlook <savas.ev@outlook.com>
2025-05-26 07:14:36 +0300

    .

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/AddressResults/GetAddressByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+
+public class GetAddressByIdQuery
+{
+    public int Id { get; set; }
+
+    public GetAddressByIdQuery(int id)
+    {
+        Id = id;
+    }
+}
