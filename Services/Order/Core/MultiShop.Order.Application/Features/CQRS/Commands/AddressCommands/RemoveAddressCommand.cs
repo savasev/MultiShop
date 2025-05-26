6d384959cae11161444cd275979e4b9a7c5ab929
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-26 08:50:39 +0300

    .

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/AddressCommands/RemoveAddressCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+
+public class RemoveAddressCommand
+{
+    public int AddressId { get; set; }
+
+    public RemoveAddressCommand(int id)
+    {
+        AddressId = id;
+    }
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+
+public class CreateAddressCommandHandler
+{
+    private readonly IRepository<Address> _addressRepository;
+
+
+}
