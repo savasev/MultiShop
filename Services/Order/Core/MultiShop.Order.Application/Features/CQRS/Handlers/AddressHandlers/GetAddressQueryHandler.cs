a27447fe60d7d7c3d4d41af8c2d803c6dabea953
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-26 09:48:18 +0300

    .

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+
+public class GetAddressQueryHandler
+{
+    #region Fields
+
+    private readonly IRepository<Address> _addressRepository;
+
+    #endregion
+
+    #region Ctor
+
+    public GetAddressQueryHandler(IRepository<Address> addressRepository)
+    {
+        _addressRepository = addressRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task<List<GetAddressQueryResult>> Handle()
+    {
+        var addresses = await _addressRepository.GetAllAsync();
+
+        return addresses.Select(x => new GetAddressQueryResult
+        {
+            AddressId = x.AddressId,
+            City = x.City,
+            Detail = x.Detail,
+            District = x.District,
+            UserId = x.UserId,
+        }).ToList();
+    }
+
+    #endregion
+}
