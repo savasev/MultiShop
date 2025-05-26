fe6ca55b573df5f61fe6e32fadb876152b8e8c53
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-26 10:05:31 +0300

    .

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+
+public class RemoveAddressCommandHandler
+{
+    #region Fields
+
+    private readonly IRepository<Address> _addressRepository;
+
+    #endregion
+
+    #region Ctor
+
+    public RemoveAddressCommandHandler(IRepository<Address> addressRepository)
+    {
+        _addressRepository = addressRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task Handle(RemoveAddressCommand removeAddressCommand)
+    {
+        var address = await _addressRepository.GetByIdAsync(removeAddressCommand.AddressId);
+
+        await _addressRepository.DeleteAsync(address);
+    }
+
+    #endregion
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/UpdateAddressCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+
+public class UpdateAddressCommandHandler
+{
+    #region Fields
+
+    private readonly IRepository<Address> _addressRepository;
+
+    #endregion
+
+    #region Ctor
+
+    public UpdateAddressCommandHandler(IRepository<Address> addressRepository)
+    {
+        _addressRepository = addressRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task Handle(UpdateAddressCommand command)
+    {
+        var address = await _addressRepository.GetByIdAsync(command.AddressId);
+
+        address.Detail = command.Detail;
+        address.City = command.City;
+        address.District = command.District;
+        address.UserId = command.UserId;
+
+        await _addressRepository.UpdateAsync(address);
+    }
+
+    #endregion
+}
