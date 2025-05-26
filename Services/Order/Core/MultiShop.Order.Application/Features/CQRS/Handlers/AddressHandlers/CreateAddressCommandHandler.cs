39002a0ccc630e3e275b68103ac81dc31c062bab
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-26 09:13:26 +0300

    .

--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -1,11 +1,38 @@
-﻿using MultiShop.Order.Application.Interfaces;
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
 using MultiShop.Order.Domain.Entities;
 
 namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 
 public class CreateAddressCommandHandler
 {
+    #region Fields
+
     private readonly IRepository<Address> _addressRepository;
 
+    #endregion
 
+    #region Ctor
+
+    public CreateAddressCommandHandler(IRepository<Address> addressRepository)
+    {
+        _addressRepository = addressRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task Handle(CreateAddressCommand createAddressCommand)
+    {
+        await _addressRepository.CreateAsync(new Address
+        {
+            City = createAddressCommand.City,
+            District = createAddressCommand.District,
+            Detail = createAddressCommand.Detail,
+            UserId = createAddressCommand.UserId,
+        });
+    }
+
+    #endregion
 }
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+
+public class GetAddressByIdQueryHandler
+{
+    #region Fields
+
+    private readonly IRepository<Address> _addressRepository;
+
+    #endregion
+
+    #region Ctor
+
+    public GetAddressByIdQueryHandler(IRepository<Address> addressRepository)
+    {
+        _addressRepository = addressRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task<GetAddressByIdQueryResult> Handle(GetAddressByIdQuery query)
+    {
+        var address = await _addressRepository.GetByIdAsync(query.Id);
+
+        return new GetAddressByIdQueryResult
+        {
+            AddressId = address.AddressId,
+            City = address.City,
+            Detail = address.Detail,
+            District = address.District,
+            UserId = address.UserId,
+        };
+    }
+
+    #endregion
+}
