583f84b3eaebe234c10735ee59bc8a26374c9633
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-05-26 14:35:39 +0300

    .

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandler/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.OrderDetailResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandler;
+
+public class GetOrderDetailByIdQueryHandler
+{
+    #region Fields
+
+    private readonly IRepository<OrderDetail> _orderDetailRepository;
+
+    #endregion
+
+    #region Ctor
+
+    public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> orderDetailRepository)
+    {
+        _orderDetailRepository = orderDetailRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+    {
+        var orderDetail = await _orderDetailRepository.GetByIdAsync(query.Id);
+        return new GetOrderDetailByIdQueryResult
+        {
+            OrderDetailId = orderDetail.OrderDetailId,
+            OrderingId = orderDetail.OrderingId,
+            ProductAmount = orderDetail.ProductAmount,
+            ProductId = orderDetail.ProductId,
+            ProductName = orderDetail.ProductName,
+            ProductPrice = orderDetail.ProductPrice,
+            ProductTotalPrice = orderDetail.ProductTotalPrice,
+        };
+    }
+
+    #endregion
+}
---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandler/RemoveOrderDetailCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandler;
+
+public class RemoveOrderDetailCommandHandler
+{
+    #region Fields
+
+    private readonly IRepository<OrderDetail> _orderDetailRepository;
+
+    #endregion
+
+    #region Ctor
+
+    public RemoveOrderDetailCommandHandler(IRepository<OrderDetail> orderDetailRepository)
+    {
+        _orderDetailRepository = orderDetailRepository;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task Handle(RemoveAddressCommand removeAddressCommand)
+    {
+        var orderDetail = await _orderDetailRepository.GetByIdAsync(removeAddressCommand.AddressId);
+
+        await _orderDetailRepository.DeleteAsync(orderDetail);
+    }
+
+    #endregion
+}
