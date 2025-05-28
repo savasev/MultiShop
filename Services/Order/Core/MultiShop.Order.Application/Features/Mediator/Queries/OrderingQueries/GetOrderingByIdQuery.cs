6f506466b7196e9273e5cbd3e4337423729f9703
savasevoutlook <savas.ev@outlook.com>
2025-05-28 20:52:28 +0300

    .

--- Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
@@ -3,6 +3,12 @@ using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
 
 namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
 
-public class GetOrderingQuery : IRequest<List<GetOrderingQueryResult>>
+public class GetOrderingByIdQuery : IRequest<GetOrderingByIdQueryResult>
 {
+    public int Id { get; set; }
+
+    public GetOrderingByIdQuery(int id)
+    {
+        Id = id;
+    }
 }
