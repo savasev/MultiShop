3529a94c0789fa150398946183fb7d7507b29a62
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-06-17 16:02:36 +0300

    .

--- IdentityServer/MultiShop.IdentityServer/Models/ApplicationUser.cs
+++ IdentityServer/MultiShop.IdentityServer/Models/ApplicationUser.cs
@@ -5,5 +5,8 @@ namespace MultiShop.IdentityServer.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
     }
 }
