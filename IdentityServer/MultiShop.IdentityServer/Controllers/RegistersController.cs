6f34c8a8d92d562c66a7a211a14a3953863fef43
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-06-17 22:26:17 +0300

    .

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.DTOs;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class RegistersController : ControllerBase
+    {
+        #region Fields
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        #endregion
+
+        #region Ctor
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        #endregion
+
+        #region Methods
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, userRegisterDto.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla eklendi");
+            }
+
+            return BadRequest("Bir hata oluştu, tekrar deneyiniz");
+        }
+
+        #endregion
+    }
+}
