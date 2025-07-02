228fc75ea0302be4290b5d7c68029c8de14f3a04
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-02 23:06:15 +0300

    .

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandler;
 using MultiShop.Order.Application.Interfaces;
@@ -12,6 +13,13 @@ namespace MultiShop.Order.WebApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.Authority = builder.Configuration["IdentityServerUrl"];
+                options.Audience = "ResourceOrder";
+                options.RequireHttpsMetadata = false;
+            });
 
             // Add services to the container.
             builder.Services.AddDbContext<OrderContext>();
@@ -51,8 +59,9 @@ namespace MultiShop.Order.WebApi
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
