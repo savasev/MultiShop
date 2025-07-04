02aa2c9a770425c77e1dca6a8dfa0fc88e3cbad1
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-04 17:10:55 +0300

    .

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Cargo.DataAccessLayer.Abstract;
+
+public interface IGenericDal<T> where T : class
+{
+    Task InsertAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeleteAsync(int id);
+
+    T GetByIdAsync(int id);
+
+    Task<List<T>> GetAllAsync();
+}
