f4228cef54b4aed3779a1b0dbb4c2e01d2dd871e
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-04 15:59:43 +0300

    .

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Concrete/CargoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.DataAccessLayer.Concrete;
+
+public class CargoContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost,1441;Initial Catalog=MultiShopCargoDb;User=sa;Password=*Q1w2e3r4t5*");
+    }
+
+    public DbSet<CargoCompany> CargoCompanies { get; set; }
+
+    public DbSet<CargoDetail> CargoDetails { get; set; }
+
+    public DbSet<CargoCustomer> CargoCustomers { get; set; }
+
+    public DbSet<CargoOperation> CargoOperations { get; set; }
+}
