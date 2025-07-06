fa6b6af8f1f8ec23923f607de34c3ff11c3c22fe
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-06 22:34:35 +0300

    .

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoCompaniesController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoCompanyDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CargoCompaniesController : ControllerBase
+{
+    #region Fields
+
+    private readonly ICargoCompanyService _cargoCompanyService;
+
+    #endregion
+
+    #region Ctor
+
+    public CargoCompaniesController(ICargoCompanyService cargoCompanyService)
+    {
+        _cargoCompanyService = cargoCompanyService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public async Task<IActionResult> CargoCompanyList()
+    {
+        var cargoCompanies = await _cargoCompanyService.GetAllAsync();
+
+        return Ok(cargoCompanies);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCargoCompanyById(int id)
+    {
+        var cargoCompany = await _cargoCompanyService.GetByIdAsync(id);
+
+        return Ok(cargoCompany);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCargoCompany(CreateCargoCompanyDto createCargoCompanyDto)
+    {
+        await _cargoCompanyService.InsertAsync(new CargoCompany
+        {
+            CargoCompanyName = createCargoCompanyDto.CargoCompanyName,
+        });
+
+        return Ok("Kargo şirketi başarıyla oluşturuldu");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateCargoCompany(UpdateCargoCompanyDto updateCargoCompanyDto)
+    {
+        await _cargoCompanyService.UpdateAsync(new CargoCompany
+        {
+            CargoCompanyId = updateCargoCompanyDto.CargoCompanyId,
+            CargoCompanyName = updateCargoCompanyDto.CargoCompanyName,
+        });
+
+        return Ok("Kargo şirketi başarıyla güncellendi.");
+    }
+
+    public async Task<IActionResult> RemoveCargoCompany(int id)
+    {
+        await _cargoCompanyService.DeleteAsync(id);
+
+        return Ok("Kargo şirketi başarıyla silindi");
+    }
+
+    #endregion
+}
