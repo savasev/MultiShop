dec020833613d373f554d875b2bd7b7fdf78a8e2
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-06 11:25:19 +0300

    .

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Cargo.BusinessLayer.Abstract;
+
+public interface IGenericService<T> where T : class
+{
+    Task InsertAsync(T entity);
+
+    Task UpdateAsync(T entity);
+
+    Task DeleteAsync(int id);
+
+    Task<T> GetByIdAsync(int id);
+
+    Task<List<T>> GetAllAsync();
+}
