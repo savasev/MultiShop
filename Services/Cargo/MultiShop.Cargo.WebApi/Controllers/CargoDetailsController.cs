7d333b3d60284eaceb4b78cad2cc246df426fbf0
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-07 08:29:16 +0300

    .

--- Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoDetailDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
 
 namespace MultiShop.Cargo.WebApi.Controllers;
 
@@ -6,5 +9,74 @@ namespace MultiShop.Cargo.WebApi.Controllers;
 [ApiController]
 public class CargoDetailsController : ControllerBase
 {
+    #region Fields
 
+    private readonly ICargoDetailService _cargoDetailService;
+
+    #endregion
+
+    #region Ctor
+
+    public CargoDetailsController(ICargoDetailService cargoDetailService)
+    {
+        _cargoDetailService = cargoDetailService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public async Task<IActionResult> CargoDetailList()
+    {
+        var cargoDetails = await _cargoDetailService.GetAllAsync();
+
+        return Ok(cargoDetails);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCargoDetailById(int id)
+    {
+        var cargoDetail = await _cargoDetailService.GetByIdAsync(id);
+
+        return Ok(cargoDetail);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCargoDetail(CreateCargoDetailDto createCargoDetailDto)
+    {
+        await _cargoDetailService.InsertAsync(new CargoDetail
+        {
+            Barcode = createCargoDetailDto.Barcode,
+            ReceiverCustomer = createCargoDetailDto.ReceiverCustomer,
+            SenderCustomer = createCargoDetailDto.SenderCustomer,
+            CargoCompanyId = createCargoDetailDto.CargoCompanyId,
+        });
+
+        return Ok("Kargo detayı başarıyla oluşturuldu");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateCargoDetail(UpdateCargoDetailDto updateCargoDetailDto)
+    {
+        await _cargoDetailService.UpdateAsync(new CargoDetail
+        {
+            CargoDetailId = updateCargoDetailDto.CargoDetailId,
+            Barcode = updateCargoDetailDto.Barcode,
+            ReceiverCustomer = updateCargoDetailDto.ReceiverCustomer,
+            SenderCustomer = updateCargoDetailDto.SenderCustomer,
+            CargoCompanyId = updateCargoDetailDto.CargoCompanyId,
+        });
+
+        return Ok("Kargo detayı başarıyla güncellendi.");
+    }
+
+    public async Task<IActionResult> RemoveCargoDetail(int id)
+    {
+        await _cargoDetailService.DeleteAsync(id);
+
+        return Ok("Kargo detayı başarıyla silindi");
+    }
+
+    #endregion
 }
