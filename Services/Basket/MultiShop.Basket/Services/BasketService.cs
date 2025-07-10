e2d5b6059100885330d9a42a000cfcff908762e3
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-10 18:28:23 +0300

    .

---
+++ Services/Basket/MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,46 @@
+﻿using MultiShop.Basket.Dtos;
+using MultiShop.Basket.Settings;
+using System.Text.Json;
+
+namespace MultiShop.Basket.Services;
+
+public class BasketService : IBasketService
+{
+    #region Fields
+
+    private readonly RedisService _redisService;
+
+    #endregion
+
+    #region Ctor
+
+    public BasketService(RedisService redisService)
+    {
+        _redisService = redisService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task DeleteBasketAsync(string userId)
+    {
+        var status = await _redisService.GetDb().KeyDeleteAsync(userId);
+
+
+    }
+
+    public async Task<BasketTotalDto> GetBasketAsync(string userId)
+    {
+        var basketTotal = await _redisService.GetDb().StringGetAsync(userId);
+
+        return JsonSerializer.Deserialize<BasketTotalDto>(basketTotal);
+    }
+
+    public Task<bool> SaveBasketAsync(BasketTotalDto basket)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
+}
---
+++ Services/Basket/MultiShop.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Basket.Dtos;
+
+namespace MultiShop.Basket.Services;
+
+public interface IBasketService
+{
+    Task<BasketTotalDto> GetBasketAsync(string userId);
+
+    Task<bool> SaveBasketAsync(BasketTotalDto basket);
+
+    Task DeleteBasketAsync(string userId);
+}
