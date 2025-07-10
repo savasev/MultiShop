c46382360d6c8a1d6575441b4ca7e787e8ae449c
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-10 17:55:03 +0300

    .

---
+++ Services/Basket/MultiShop.Basket/Settings/RedisService.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+
+namespace MultiShop.Basket.Settings;
+
+public class RedisService
+{
+    #region Fields
+
+    private readonly string _host;
+    private readonly int _port;
+    private ConnectionMultiplexer _connectionMultiplexer;
+
+    #endregion
+
+    #region Ctor
+
+    public RedisService(string host, int port)
+    {
+        _host = host;
+        _port = port;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_host}:{_port}");
+
+    public IDatabase GetDb(int db = 1) => _connectionMultiplexer.GetDatabase(0);
+
+    #endregion
+}
