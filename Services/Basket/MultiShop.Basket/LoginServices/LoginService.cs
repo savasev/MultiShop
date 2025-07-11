eaec3a7a7a87b4c6f03c4fe389029d848ad3c619
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-11 10:47:47 +0300

    .

---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,25 @@
+﻿namespace MultiShop.Basket.LoginServices;
+
+public class LoginService : ILoginService
+{
+    #region Fields
+
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    #endregion
+
+    #region Ctor
+
+    public LoginService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public string GetUserId => _httpContextAccessor.HttpContext.User.FindFirst("sub").Value;
+
+    #endregion
+}
