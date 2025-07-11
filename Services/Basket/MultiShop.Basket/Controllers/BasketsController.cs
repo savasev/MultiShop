542ce5fbca4f9339128e725a082062c9582214c0
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-07-11 14:14:12 +0300

    .

--- Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
 
 namespace MultiShop.Basket.Controllers;
 
@@ -6,5 +9,50 @@ namespace MultiShop.Basket.Controllers;
 [ApiController]
 public class BasketsController : ControllerBase
 {
+    #region Fields
 
+    private readonly IBasketService _basketService;
+    private readonly ILoginService _loginService;
+
+    #endregion
+
+    #region Ctor
+
+    public BasketsController(IBasketService basketService,
+        ILoginService loginService)
+    {
+        _basketService = basketService;
+        _loginService = loginService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public async Task<IActionResult> GetBasket()
+    {
+        var values = await _basketService.GetBasketAsync(_loginService.GetUserId);
+        return Ok(values);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SaveBasket(BasketTotalDto basketTotalDto)
+    {
+        basketTotalDto.UserId = _loginService.GetUserId;
+
+        await _basketService.SaveBasketAsync(basketTotalDto);
+
+        return Ok("Sepetteki değişiklikler kaydedildi.");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBasket()
+    {
+        await _basketService.DeleteBasketAsync(_loginService.GetUserId);
+
+        return Ok("Sepet başarıyla silindi.");
+    }
+
+    #endregion
 }
