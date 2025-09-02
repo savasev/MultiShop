633b3fb17b96a6a3febf9107d8cb0a352bdeb2e4
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-02 15:30:34 +0300

    .

---
+++ Frontend/MultiShop.WebUI/ViewComponents/ProductDetailsFeaturedProductsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents;
+
+public class ProductDetailsFeaturedProductsViewComponent : ViewComponent
+{
+    #region Methods
+
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+
+    #endregion
+}
