774f8d0e546ec86e9c918875e442bb8df6cba81d
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-02 10:42:45 +0300

    .

---
+++ Frontend/MultiShop.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers;
+
+public class ProductController : Controller
+{
+    #region Methods
+
+    public IActionResult Index()
+    {
+        return RedirectToAction("List");
+    }
+
+    public IActionResult List()
+    {
+        return View();
+    }
+
+    #endregion
+}
