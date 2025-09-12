bc9344bc253d022b78cd53066d7a643045084a3d
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-12 19:02:49 +0300

    .

--- Frontend/MultiShop.WebUI/Components/CarouselViewComponent.cs
+++ Frontend/MultiShop.WebUI/Components/CarouselViewComponent.cs
@@ -1,14 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Components;
 
 public class CarouselViewComponent : ViewComponent
 {
+    #region Fields
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    #endregion
+
+    #region Ctor
+
+    public CarouselViewComponent(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    #endregion
+
     #region Methods
 
-    public IViewComponentResult Invoke()
+    public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View();
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync("https://localhost:7070/api/featuresliders");
+
+        if (!response.IsSuccessStatusCode)
+            return Content(string.Empty);
+
+        var jsonData = await response.Content.ReadAsStringAsync();
+        var featureSliders = JsonConvert.DeserializeObject<List<FeatureSliderModel>>(jsonData);
+
+        return View(featureSliders);
     }
 
     #endregion
---
+++ Frontend/MultiShop.WebUI/Models/FeatureSliderModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MultiShop.WebUI.Models;
+
+public class FeatureSliderModel
+{
+    #region Properties
+
+    public string FeatureSliderId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    #endregion
+}
