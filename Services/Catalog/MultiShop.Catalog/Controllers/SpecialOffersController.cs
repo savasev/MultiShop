46a12401a23b226aa49bcb8615872d0813a2ff7e
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-12 20:58:25 +0300

    .

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.SpecialOfferDtos;
+using MultiShop.Catalog.Services.SpecialOfferServices;
+
+namespace MultiShop.Catalog.Controllers;
+
+public class SpecialOffersController : BaseApiController
+{
+    #region Fields
+
+    private readonly ISpecialOfferService _specialOfferService;
+
+    #endregion
+
+    #region Ctor
+
+    public SpecialOffersController(ISpecialOfferService specialOfferService)
+    {
+        _specialOfferService = specialOfferService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public async Task<IActionResult> GetSpecialOfferList()
+    {
+        var specialOffers = await _specialOfferService.GetAllSpecialOffersAsync();
+
+        return Ok(specialOffers);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetSpecialOfferById(string id)
+    {
+        var specialOffer = await _specialOfferService.GetSpecialOfferByIdAsync(id);
+
+        return Ok(specialOffer);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDto createSpecialOfferDto)
+    {
+        await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDto);
+
+        return Ok("Özel teklif başarıyla eklendi.");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDto updateSpecialOfferDto)
+    {
+        await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDto);
+
+        return Ok("Özel teklif başarıyla güncellendi.");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteSpecialOffer(string id)
+    {
+        await _specialOfferService.DeleteSpecialOfferAsync(id);
+
+        return Ok("Özel teklif başarıyla silindi.");
+    }
+
+    #endregion
+}
