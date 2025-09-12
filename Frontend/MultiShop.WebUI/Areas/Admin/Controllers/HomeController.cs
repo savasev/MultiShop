70e2f2532c0ccd1b2d2e01eb6363fca3289547ba
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-12 20:06:47 +0300

    .

---
+++ Frontend/MultiShop.WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers;
+
+public class HomeController : BaseAdminController
+{
+    #region Methods
+
+    public IActionResult Index()
+    {
+        return RedirectToAction("Dashboard");
+    }
+
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    #endregion
+}
