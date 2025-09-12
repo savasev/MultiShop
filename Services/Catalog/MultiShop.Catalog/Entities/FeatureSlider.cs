0c380d0d882ed0c34cca49437864cb0ef73df8e6
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-12 16:42:57 +0300

    .

--- Services/Catalog/MultiShop.Catalog/Entities/FeatureSlider.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/FeatureSlider.cs
@@ -1,10 +1,12 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MultiShop.Catalog.Entities;
 
 public class FeatureSlider
 {
     [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public string FeatureSliderId { get; set; }
 
     public string Title { get; set; }
