bf4e544050fe8ea669567fda2539c111c9c04636
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-14 23:27:31 +0300

    .

--- Frontend/MultiShop.WebUI/Components/CategoriesViewComponent.cs
+++ Frontend/MultiShop.WebUI/Components/CategoriesViewComponent.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Components;
 
 public class CategoriesViewComponent : ViewComponent
 {
+    #region Fields
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    #endregion
+
+    #region Ctor
+
+    public CategoriesViewComponent(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    #endregion
+
     #region Methods
 
-    public IViewComponentResult Invoke()
+    public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View();
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync("https://localhost:7070/api/categories");
+
+        if (!response.IsSuccessStatusCode)
+            return Content(string.Empty);
+
+        var jsonData = await response.Content.ReadAsStringAsync();
+        var categories = JsonConvert.DeserializeObject<List<CategoryModel>>(jsonData)
+            ?? new List<CategoryModel>();
+
+        return View(categories);
     }
 
     #endregion
---
+++ Frontend/MultiShop.WebUI/Models/CategoryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.WebUI.Models;
+
+public class CategoryModel
+{
+    #region Properties
+
+    public string CategoryName { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    #endregion
+}
