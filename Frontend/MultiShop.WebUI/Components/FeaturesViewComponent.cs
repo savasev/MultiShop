8ef645239eafbc6c45d39ea1b91a6c973936cc29
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-14 15:28:05 +0300

    .

---
+++ Frontend/MultiShop.WebUI/Components/FeaturesViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models;
+using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.Components;
+
+public class FeaturesViewComponent : ViewComponent
+{
+    #region Fields
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    #endregion
+
+    #region Ctor
+
+    public FeaturesViewComponent(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync("https://localhost:7070/api/features");
+
+        if (!response.IsSuccessStatusCode)
+            return Content(string.Empty);
+
+        var jsonData = await response.Content.ReadAsStringAsync();
+        var features = JsonConvert.DeserializeObject<List<FeatureModel>>(jsonData)
+            ?? new List<FeatureModel>();
+
+        return View(features);
+    }
+
+    #endregion
+}
---
+++ Frontend/MultiShop.WebUI/Models/FeatureModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.WebUI.Models;
+
+public class FeatureModel
+{
+    #region Properties
+
+    public string Title { get; set; }
+
+    public string Icon { get; set; }
+
+    #endregion
+}
