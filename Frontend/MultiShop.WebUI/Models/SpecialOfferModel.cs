011b87210b88201002af035667ac86b97b09ec2d
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-14 11:20:37 +0300

    .

---
+++ Frontend/MultiShop.WebUI/Models/SpecialOfferModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.WebUI.Models;
+
+public class SpecialOfferModel
+{
+    #region Properties
+
+    public string Title { get; set; }
+
+    public string SubTitle { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    #endregion
+}
