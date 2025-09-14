b92cc1d9f729eac15efaf8dc37f5898712359a0d
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-14 12:38:46 +0300

    .

--- Frontend/MultiShop.WebUI/Components/OfferDiscountViewComponent.cs
+++ Frontend/MultiShop.WebUI/Components/OfferDiscountViewComponent.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Components;
 
 public class OfferDiscountViewComponent : ViewComponent
 {
+    #region Fields
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    #endregion
+
+    #region Ctor
+
+    public OfferDiscountViewComponent(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    #endregion
+
     #region Methods
 
-    public IViewComponentResult Invoke()
+    public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View();
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync("https://localhost:7070/api/specialoffers");
+
+        if (!response.IsSuccessStatusCode)
+            return Content(string.Empty);
+
+        var jsonData = await response.Content.ReadAsStringAsync();
+        var specialOffers = JsonConvert.DeserializeObject<List<SpecialOfferModel>>(jsonData)
+            ?? new List<SpecialOfferModel>();
+
+        return View(specialOffers);
     }
 
     #endregion
