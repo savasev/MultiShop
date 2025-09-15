5c1c32458aa1e4fc15cbe21e31e54cba83ce0cab
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-15 21:42:44 +0300

    .

--- Frontend/MultiShop.WebUI/Components/FeaturedProductsViewComponent.cs
+++ Frontend/MultiShop.WebUI/Components/FeaturedProductsViewComponent.cs
@@ -1,14 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Models;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.Components;
 
 public class FeaturedProductsViewComponent : ViewComponent
 {
+    #region Fields
+
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    #endregion
+
+    #region Ctor
+
+    public FeaturedProductsViewComponent(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    #endregion
+
     #region Methods
 
-    public IViewComponentResult Invoke()
+    public async Task<IViewComponentResult> InvokeAsync()
     {
-        return View();
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync("https://localhost:7070/api/products/products");
+
+        if (!response.IsSuccessStatusCode)
+            return Content(string.Empty);
+
+        var jsonData = await response.Content.ReadAsStringAsync();
+        var products = JsonConvert.DeserializeObject<List<ProductModel>>(jsonData)
+            ?? new List<ProductModel>();
+
+        return View(products);
     }
 
     #endregion
---
+++ Frontend/MultiShop.WebUI/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.WebUI.Models;
+
+public class ProductModel
+{
+    #region Properties
+
+    public string ProductName { get; set; }
+
+    public decimal ProductPrice { get; set; }
+
+    public string ProductImageUrl { get; set; }
+
+    #endregion
+}
