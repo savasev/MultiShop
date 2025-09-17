492f86329584f2d6fa71818ad71ed5edf80174e5
savasevoutlook <87226552+savasevoutlook@users.noreply.github.com>
2025-09-17 11:14:17 +0300

    .

--- Services/Catalog/MultiShop.Catalog/Controllers/GeneralSettingsController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/GeneralSettingsController.cs
@@ -1,12 +1,15 @@
-﻿using MultiShop.Catalog.Services.GeneralSettingServices;
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.DTOs.GeneralSettingDtos;
+using MultiShop.Catalog.Entities.Enums;
+using MultiShop.Catalog.Services.GeneralSettingServices;
 
 namespace MultiShop.Catalog.Controllers;
 
 public class GeneralSettingsController : BaseApiController
 {
-	#region Fields
+    #region Fields
 
-	private readonly IGeneralSettingService _generalSettingService;
+    private readonly IGeneralSettingService _generalSettingService;
 
     #endregion
 
@@ -21,7 +24,58 @@ public class GeneralSettingsController : BaseApiController
 
     #region Methods
 
+    [HttpGet]
+    public async Task<IActionResult> GetGeneralSettingList()
+    {
+        var generalSettings = await _generalSettingService.GetAllGeneralSettingsAsync();
 
+        return Ok(generalSettings);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetGeneralSettingById(string id)
+    {
+        var generalSetting = await _generalSettingService.GetGeneralSettingByIdAsync(id);
+
+        return Ok(generalSetting);
+    }
+
+    [HttpGet("settingcategories")]
+    public IActionResult GetCategories()
+    {
+        var categories = Enum.GetValues(typeof(SettingCategory)).Cast<SettingCategory>()
+            .Select(category => new
+            {
+                Id = (int)category,
+                Name = category.ToString()
+            });
+
+        return Ok(categories);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateGeneralSetting(CreateGeneralSettingDto createGeneralSettingDto)
+    {
+        await _generalSettingService.CreateGeneralSettingAsync(createGeneralSettingDto);
+
+        return Ok("Ayar başarıyla eklendi.");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateGeneralSetting(UpdateGeneralSettingDto updateGeneralSettingDto)
+    {
+        await _generalSettingService.UpdateGeneralSettingAsync(updateGeneralSettingDto);
+
+        return Ok("Ayar başarıyla güncellendi.");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteGeneralSetting(string id)
+    {
+        await _generalSettingService.DeleteGeneralSettingAsync(id);
+
+        return Ok("Ayar başarıyla silindi.");
+    }
 
     #endregion
 }
